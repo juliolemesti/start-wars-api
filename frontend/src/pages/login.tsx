fee23951c788bc38edb6b236282fda7c20fca2b7
Julio Lemes <juliolemesti@gmail.com>
2025-09-04 18:52:38 -0300

    add  react-router

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+export const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Login Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/starships.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+export const StarshipsPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Starships Page</h1>
+    </div>
+  );
+}
